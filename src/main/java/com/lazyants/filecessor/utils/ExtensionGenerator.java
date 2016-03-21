46b1d3f7b422f10db74c67aa1796f7084c90af60
dimka2014 <dimabelyaev27@gmail.com>
2016-03-21 18:35:00 +0200

    resize by coordinates

--- src/main/java/com/lazyants/filecessor/utils/ExtensionGenerator.java
+++ src/main/java/com/lazyants/filecessor/utils/ExtensionGenerator.java
@@ -1,17 +1,29 @@
 package com.lazyants.filecessor.utils;
 
+import org.springframework.http.MediaType;
+
 public class ExtensionGenerator {
     public static String getExtension(String contentType) {
         switch (contentType) {
             case "image/gif":
-
                 return "gif";
             case "image/png":
-
                 return "png";
             case "image/jpeg":
-
                 return "jpg";
+        }
+
+        return null;
+    }
+
+    public static MediaType getMediaType(String extension) {
+        switch (extension) {
+            case "jpg":
+                return MediaType.IMAGE_JPEG;
+            case "png":
+                return MediaType.IMAGE_PNG;
+            case "gif":
+                return MediaType.IMAGE_GIF;
         }
 
         return null;
